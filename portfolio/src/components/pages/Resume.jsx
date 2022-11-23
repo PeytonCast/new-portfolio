51558e954a161df9e5f63ba3c155af22e9fd9a21
Peyton C <peyton.castill0@icloud.com>
2022-11-23 01:35:08 -0500

    styled the resume page

--- portfolio/src/components/pages/Resume.jsx
+++ portfolio/src/components/pages/Resume.jsx
@@ -3,10 +3,14 @@ import pcresume from "../../assets/pcresume.pdf"
 import React from "react";
 function Resume () {
     return (
-        <div>
+        <div className="card m-2">
+            <div class="card-header">
             <h3>Resume</h3>
-            <p>Link to downloadabe resume.</p>
-            <a href={pcresume} download="Peyton_Castillo_Resume">click here to download</a>
+             </div>
+            <div className="card-body">
+             <p className="card-text">Link to downloadabe resume.</p>
+             <a href={pcresume} download="Peyton_Castillo_Resume" className="badge badge-success p-2">click here to download</a>
+            </div>
         </div>
     )
 }

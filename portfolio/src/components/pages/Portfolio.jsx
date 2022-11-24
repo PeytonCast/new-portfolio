44bcaaea6d95dfe45cb46c0fdd14f4c5c7bab8dc
Peyton C <peyton.castill0@icloud.com>
2022-11-23 23:18:57 -0500

    adding logos to skills list

--- portfolio/src/components/pages/Portfolio.jsx
+++ portfolio/src/components/pages/Portfolio.jsx
@@ -13,60 +13,63 @@ function Portfolio () {
     return (
         <div className="container">
             <h3>Portfolio</h3>
-            <div className="row justify-content-center">
-            {/* BLOG SPOT */}
-            <section className="card shadow my-2" style={{width: "12rem"}}>
-            <img src="https://via.placeholder.com/150" alt="Portfolio" />
-            <div className="p-2">
-            <p><a href="https://github.com/PeytonCast/Blog-Spot">Blog-Spot (repository)</a></p>
-            <p><a href="https://blog-spot-3.herokuapp.com/">Blog-Spot(website)</a></p>
-            </div> 
-            </section>
+            <div className="d-flex flex-wrap justify-content-around mb-5">
+            {/* <div> */}
+                    {/* BLOG SPOT */}
+                    <section className="card shadow my-2" style={{width: "12rem"}}>
+                    <img src="https://via.placeholder.com/150" alt="Portfolio" />
+                    <div className="p-2">
+                    <p><a href="https://github.com/PeytonCast/Blog-Spot">Blog-Spot (repository)</a></p>
+                    <p><a href="https://blog-spot-3.herokuapp.com/">Blog-Spot(website)</a></p>
+                    </div> 
+                    </section>
 
-            {/* THIS APP */}
-            <section className="card shadow my-2" style={{width: "12rem"}}>
-            <img src="https://via.placeholder.com/150" alt="Portfolio" />
-            <div className="p-2">
-            <p><a href="https://github.com/PeytonCast/new-portfolio">This Portfolio's Repository</a></p>
-            </div> 
-            </section>
+                    {/* THIS APP */}
+                    <section className="card shadow my-2" style={{width: "12rem"}}>
+                    <img src="https://via.placeholder.com/150" alt="Portfolio" />
+                    <div className="p-2">
+                    <p><a href="https://github.com/PeytonCast/new-portfolio">This Portfolio's Repository</a></p>
+                    </div> 
+                    </section>
 
-            {/* TEXT EDITOR PWA */}
-            <section className="card shadow my-2" style={{width: "12rem"}}>
-            <img src="https://via.placeholder.com/150" alt="Text editor" />
-            <div className="p-2">
-                <p><a href="https://github.com/PeytonCast/PWA-text-editor">Text Editor PWA (repository)</a></p>
-                <p><a href="https://gt12-3.herokuapp.com/">Text Editor PWA (site)</a></p>
-            </div> 
-            </section>
+                    {/* TEXT EDITOR PWA */}
+                    <section className="card shadow my-2" style={{width: "12rem"}}>
+                    <img src="https://via.placeholder.com/150" alt="Text editor" />
+                    <div className="p-2">
+                        <p><a href="https://github.com/PeytonCast/PWA-text-editor">Text Editor PWA (repository)</a></p>
+                        <p><a href="https://gt12-3.herokuapp.com/">Text Editor PWA (site)</a></p>
+                    </div> 
+                    </section>
+            {/* </div>
+            <div> */}
+                    {/*  RECIPE FINDER */}
+                    <section className="card shadow my-2" style={{width: "12rem"}}>
+                    <img src="https://via.placeholder.com/150" alt="Text editor" />
+                    <div className="p-2">
+                        <p><a href="https://github.com/PeytonCast/Recipe-finder">Recipe Finder (repository)</a></p>
+                        <p><a href="https://peytoncast.github.io/Project-1-Recipe-finder/">Recipe Finder (site)</a></p>
+                    </div> 
+                    </section>
+                    
+                    {/* PASSWORD GENERATOR */}
+                    <section className="card shadow my-2" style={{width: "12rem"}}>
+                    <img src="https://via.placeholder.com/150" alt="Text editor" />
+                    <div className="p-2">
+                        <p><a href="https://github.com/PeytonCast/password_generator">Password Generator (repository)</a></p>
+                        <p><a href="https://peytoncast.github.io/password_generator/Develop/">Password Generator (site)</a></p>
+                    </div> 
+                    </section>
 
-            {/*  RECIPE FINDER */}
-            <section className="card shadow my-2" style={{width: "12rem"}}>
-            <img src="https://via.placeholder.com/150" alt="Text editor" />
-            <div className="p-2">
-                <p><a href="https://github.com/PeytonCast/Recipe-finder">Recipe Finder (repository)</a></p>
-                <p><a href="https://peytoncast.github.io/Project-1-Recipe-finder/">Recipe Finder (site)</a></p>
-            </div> 
-            </section>
-            
-            {/* PASSWORD GENERATOR */}
-            <section className="card shadow my-2" style={{width: "12rem"}}>
-            <img src="https://via.placeholder.com/150" alt="Text editor" />
-            <div className="p-2">
-                <p><a href="https://github.com/PeytonCast/password_generator">Password Generator (repository)</a></p>
-                <p><a href="https://peytoncast.github.io/password_generator/Develop/">Password Generator (site)</a></p>
-            </div> 
-            </section>
-
-            {/* A-SCARY-QUIZ */}
-            <section className="card shadow my-2" style={{width: "12rem"}}>
-            <img src="https://via.placeholder.com/150" alt="Text editor" />
-            <div className="p-2">
-                <p><a href="https://github.com/PeytonCast/A-SCARY-QUIZ">A-SCARY-QUIZ (repository)</a></p>
-                <p><a href="https://peytoncast.github.io/A-SCARY-QUIZ/Assets/index1.html">A-SCARY-QUIZ (site)</a></p>
-            </div> 
-            </section>
-            </div>
+                    {/* A-SCARY-QUIZ */}
+                    <section className="card shadow my-2" style={{width: "12rem"}}>
+                    <img src="https://via.placeholder.com/150" alt="Text editor" />
+                    <div className="p-2">
+                        <p><a href="https://github.com/PeytonCast/A-SCARY-QUIZ">A-SCARY-QUIZ (repository)</a></p>
+                        <p><a href="https://peytoncast.github.io/A-SCARY-QUIZ/Assets/index1.html">A-SCARY-QUIZ (site)</a></p>
+                    </div> 
+                    </section>
+                </div>
+            {/* </div> */}
         </div>
     )
 }

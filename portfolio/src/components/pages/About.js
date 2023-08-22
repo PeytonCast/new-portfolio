01abf1764791ee947e079398cc58b7f90e590c08
Peyton C <peyton.castill0@icloud.com>
2023-08-21 21:34:38 -0400

    updated the icons

--- portfolio/src/components/pages/About.js
+++ portfolio/src/components/pages/About.js
@@ -1,6 +1,16 @@
 // I see a recent photo or avatar of the developer and a short bio about them
 import React from "react";
 import '../../styles/app.css'
+import JSicon from "../../assets/img/js-port-icon.jpg";
+import ReactIcon from "../../assets/img/react_icon.webp"
+import BootStrapIcon from "../../assets/img/bootstrap-icon.png"
+import GoogleIcon from "../../assets/img/Google-Analytics-Emblem.png"
+import MySQLIcon from "../../assets/img/MySQL-Icon.png"
+import MongoIcon from "../../assets/img/mongo-icon.png"
+import nodeJS from "../../assets/img/nodejs-icon.png"
+import expressIcon from "../../assets/img/expressjs.png"
+
+
 function About () {
     return (
         <div>
@@ -19,26 +29,23 @@ function About () {
                where I contribute to the education of students enrolled in bootcamps for the esteemed Georgia Institute of Technology. 
                The gratification I derive from sharing knowledge and facilitating the transformation of talented students into seasoned developers is immeasurable.
                <br/>
-               When I am not actively engaged in teaching,
-               I allocate my time towards executing client or personal projects. 
-               My favorite is a long-term project I am presently undertaking, which involves the creation of a Tech Blog scheduled for launch in early 2023. 
-               Through this platform, I intend to document my journey through the tech field and address issues such as AI and new languages.
+               
             </p>
             <div className="">
                     {/* need help with this  style={{width: "12rem"}}*/}
                     <ul className="row ml-2 mr-2 mb-5">
                             <li className="col-sm-12 col-md-4 skills text-light mb-2"><h5>Skills</h5></li>
-                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "2rem"}}src="https://th.bing.com/th/id/R.e6d82362f50c26c1b0e6186b99e18e3c?rik=ZkiNhqWudt2EYQ&riu=http%3a%2f%2f3.bp.blogspot.com%2f-PTty3CfTGnA%2fTpZOEjTQ_WI%2fAAAAAAAAAeo%2fKeKt_D5X2xo%2fs1600%2fjs.jpg&ehk=uLz7cG48BbXqyb7LS1GN3v3uFyOhkFlskPBBnFx4ryc%3d&risl=&pid=ImgRaw&r=0" alt="JavaScript" /> JavaScript</li>
-                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "2rem"}}src="https://th.bing.com/th/id/OIP.HrBOk_qD-pQSmgSI3XzRJAHaHw?w=160&h=180&c=7&r=0&o=5&pid=1.7" alt="Python" /> Python</li>
-                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "2rem"}}src="https://th.bing.com/th/id/R.91d202ff3335dd331e452965fdb7b966?rik=I0wd%2bnwSC1x5%2fQ&riu=http%3a%2f%2ffresheropenings.com%2fwp-content%2fuploads%2f2020%2f10%2fFree-C-Certification-Course-1-696x696.png&ehk=XBdGIt%2bkX0vtBUWVtNmBOm9JzAvcTopZP7ZUPwYQKmM%3d&risl=&pid=ImgRaw&r=0" alt="C++" /> C++</li>
-                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "1.7rem"}}src="https://egenteas.sirv.com/exato/project/react_icon.png" alt="React" /> React</li>
-                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "2rem"}}src="https://th.bing.com/th/id/OIP.mNLHMt-PM0iPB52LqWOBAgHaHd?pid=ImgDet&rs=1" alt="Bootstrap" /> BootStrap</li>
-                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "2.2rem"}}src="https://logos-world.net/wp-content/uploads/2021/02/Google-Analytics-Emblem.png" alt="GA4" /> Google Analytics </li>
-                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "2.8rem"}}src="https://th.bing.com/th/id/R.6b47d7fec15d3e1a5de086ac1c808f6d?rik=IIkC39481Md3Bw&riu=http%3a%2f%2flogos-download.com%2fwp-content%2fuploads%2f2016%2f05%2fMySQL_logo_logotype.png&ehk=aWHquyoObU%2fXSsDiw7VKaqGdBCxP2cRjipdNUO5Q6us%3d&risl=&pid=ImgRaw&r=0" alt="MySql" />MySQL</li>
+                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "2rem"}}src={JSicon} alt="JavaScript" /> JavaScript</li>
+                            {/* <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "2rem"}}src="" alt="Python" /> Python</li> */}
+                            {/* <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "2rem"}}src="" alt="C++" /> C++</li> */}
+                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "1.7rem"}} src={ReactIcon} alt="React" /> React</li>
+                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "2rem"}}src={BootStrapIcon} alt="Bootstrap" /> BootStrap</li>
+                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "2.2rem"}}src={GoogleIcon} alt="GA4" /> Google Analytics </li>
+                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "2rem"}}src={MySQLIcon} alt="MySql" />MySQL</li>
 
-                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "2rem"}}src="https://basesdedatosactividad2.files.wordpress.com/2014/08/mongo-db-alternate-logo.png" alt="mongoDB" /> MongoDB</li>
-                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "2rem"}}src="https://i.pinimg.com/originals/50/d0/83/50d0834e3e146ad945ee8b4f89b5b309.png" alt="node" /> Node.js</li>
-                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "2rem"}}src="https://th.bing.com/th/id/OIP.1fZjQpkRMKTBGN_7H5YnFwHaGL?pid=ImgDet&rs=1" alt="express.js" /> Express.js</li>
+                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "2rem"}}src={MongoIcon} alt="mongoDB" /> MongoDB</li>
+                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "2rem"}}src={nodeJS} alt="node" /> Node.js</li>
+                            <li className="col-sm-12 col-md-4 font-weight-light mb-2"><img style={{width: "1.8rem"}}src={expressIcon} alt="express.js" /> Express.js</li>
                     </ul>
 
             </div>
